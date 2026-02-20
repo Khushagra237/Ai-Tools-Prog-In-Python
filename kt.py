e82b150cb52326ff6ce0bebf673769df47b090b7
Khushagra237 <148641541+Khushagra237@users.noreply.github.com>
2026-02-21 00:41:05 +0530

    Add files via upload

---
+++ kt.py
@@ -0,0 +1,10 @@
+e = int(input("enter marks"))
+h = int(input("enter marks"))
+m = int(input("enter marks"))
+s = int(input("enter marks"))
+so = int(input("enter marks"))
+sans = int(input("enter marks"))
+t = e+h+m+s+so+sans
+p = t/6
+print("Total is", t)
+print("Percentage is", p)
